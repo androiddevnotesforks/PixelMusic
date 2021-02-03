3054ebef411b585751580eb82979506e8d214e42
Kyant2021 <kyant2021@outlook.com>
2021-02-03 11:36:12 +0800

    Fade in animation for cover

--- app/src/main/java/com/kyant/pixelmusic/ui/component/Cover.kt
+++ app/src/main/java/com/kyant/pixelmusic/ui/component/Cover.kt
@@ -1,20 +1,28 @@
 package com.kyant.pixelmusic.ui.component
 
+import androidx.compose.animation.core.Animatable
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.asImageBitmap
 import com.kyant.pixelmusic.media.Song
 import com.kyant.pixelmusic.util.EmptyImage
+import com.kyant.pixelmusic.util.LaunchedIOEffectUnit
 
 @Composable
 fun Cover(
     song: Song,
     modifier: Modifier = Modifier
 ) {
+    val alpha = remember(song.icon) { Animatable(0f) }
+    song.icon?.LaunchedIOEffectUnit {
+        alpha.animateTo(1f)
+    }
     Image(
         song.icon?.asImageBitmap() ?: EmptyImage,
         song.title.toString(),
-        modifier
+        modifier.alpha(alpha.value)
     )
 }
